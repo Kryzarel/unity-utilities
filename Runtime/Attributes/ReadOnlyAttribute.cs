fe2c9096c35583a24b4f5ec9ca7304fbacbe1e66
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2024-09-21 16:26:54 +0100

    Deleted ReadOnlyDecorator hack. Since Unity 2023.3 (aka Unity 6) we now FINALLY have support for attributes that apply to whole collections, rather than just individual items inside the collection.

--- Runtime/Attributes/ReadOnlyAttribute.cs
+++ Runtime/Attributes/ReadOnlyAttribute.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 namespace Kryz.EditorUtils
 {
-	public class ReadOnlyAttribute : PropertyAttribute { }
+	public class ReadOnlyAttribute : PropertyAttribute
+	{
+		// Since Unity 2023.3 (aka Unity 6) we FINALLY have support for attributes that apply to the entire collection,
+		// rather than only to the individual items inside the collection.
+#if UNITY_2023_3_OR_NEWER
+		public ReadOnlyAttribute() : base(applyToCollection: true) { }
+#endif
+	}
 }
--- Runtime/Attributes/ReadOnlyDecoratorAttribute.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using UnityEngine;
-
-namespace Kryz.EditorUtils
-{
-	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
-	public class ReadOnlyDecoratorAttribute : PropertyAttribute { }
-}
