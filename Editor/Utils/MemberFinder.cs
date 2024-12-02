e8bcb46f35e9bc4d6477229105a433253ac835c8
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2024-12-02 00:56:34 +0000

    Added support for finding members that can be implicitly cast to the target type

--- Editor/Utils/MemberFinder.cs
+++ Editor/Utils/MemberFinder.cs
@@ -2,20 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Kryz.Utils;
 
 namespace Kryz.UnityUtils.Editor
 {
 	public static class MemberFinder
 	{
-		public static MemberInfo[] FindMembers(this Type type, BindingFlags bindingFlags, MemberTypes memberTypes, Type filterType, bool allowDerived)
+		private class FilterData
 		{
-			MemberInfo[] members = type.FindMembers(memberTypes, bindingFlags, MemberFilter, new Tuple<Type, bool>(filterType, allowDerived));
+			public readonly Type Type;
+			public readonly bool AllowDerived;
+			public readonly bool AllowImplictCast;
+
+			public FilterData(Type Type, bool allowDerived, bool allowImplictCast)
+			{
+				this.Type = Type;
+				AllowDerived = allowDerived;
+				AllowImplictCast = allowImplictCast;
+			}
+		}
+
+		public static MemberInfo[] FindMembers(this Type type, BindingFlags bindingFlags, MemberTypes memberTypes, Type filterType, bool allowDerived, bool allowImplictCast)
+		{
+			MemberInfo[] members = type.FindMembers(memberTypes, bindingFlags, MemberFilter, new FilterData(filterType, allowDerived, allowImplictCast));
 			return members.Where(m => !m.IsDuplicate(members)).ToArray();
 		}
 
 		private static bool MemberFilter(MemberInfo member, object? filterCriteria)
 		{
-			return filterCriteria is Tuple<Type, bool> data && member.IsTypeMatch(data.Item1, data.Item2);
+			if (filterCriteria is FilterData data)
+			{
+				Type memberType = member.GetMemberType();
+				return memberType.IsTypeMatch(data.Type, data.AllowDerived) || memberType.IsCastable(data.Type, data.AllowImplictCast);
+			}
+			return false;
 		}
 
 		private static bool IsDuplicate(this MemberInfo member, IReadOnlyList<MemberInfo> memberInfos)
@@ -33,9 +53,14 @@ namespace Kryz.UnityUtils.Editor
 			return false;
 		}
 
-		private static bool IsTypeMatch(this MemberInfo member, Type type, bool allowDerived)
+		private static bool IsTypeMatch(this Type memberType, Type type, bool allowDerived)
 		{
-			return allowDerived ? type.IsAssignableFrom(member.GetMemberType()) : type == member.GetMemberType();
+			return allowDerived ? type.IsAssignableFrom(memberType) : type == memberType;
+		}
+
+		private static bool IsCastable(this Type memberType, Type type, bool allowImplictCast)
+		{
+			return allowImplictCast && memberType.IsImplicitlyCastableTo(type);
 		}
 
 		private static Type GetMemberType(this MemberInfo memberInfo)
